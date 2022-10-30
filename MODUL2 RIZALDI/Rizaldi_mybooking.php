875c725a0ab24a0a2c38ee30bb9651d53662a112
rizaldiff <115601479+rizaldiff@users.noreply.github.com>
2022-10-30 21:01:54 +0700

    Update Rizaldi_mybooking.php

--- MODUL2 RIZALDI/Rizaldi_mybooking.php
+++ MODUL2 RIZALDI/Rizaldi_mybooking.php
@@ -25,9 +25,6 @@
         <li class="nav-item">
           <a class="nav-link" href="Rizaldi_booking.php">Booking</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="Rizaldi_mybooking.php">MyBooking</a>
-        </li>
       </ul>
     </div>
   </div>
